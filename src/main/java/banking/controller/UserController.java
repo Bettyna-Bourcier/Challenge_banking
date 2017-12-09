e61f7c2f9c35ba5868d47aa718f5addb2755393d
bettyna-bourcier <bettyna.bourcier@gmail.com>
2017-12-09 17:33:13 +0100

    Get user by its client number API

--- src/main/java/banking/controller/UserController.java
+++ src/main/java/banking/controller/UserController.java
@@ -5,11 +5,11 @@ import banking.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.util.List;
 
 @Controller
 public class UserController {
@@ -17,11 +17,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // Retrieve all users
-    @RequestMapping(value = "/users", method = RequestMethod.GET)
-    public ResponseEntity<List<User>> listAllUsers() {
-        List<User> users = userService.findAllUsers();
-        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+    // Retrieve a user and its outgoings by its client number
+    @RequestMapping(value = "/me", method = RequestMethod.GET)
+    public ResponseEntity<User> getUserByClientNumber() {
+        Object clientNumber = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userService.findByClientNumber(clientNumber.toString());
+        return new ResponseEntity<User>(user, HttpStatus.OK);
     }
 
 }
